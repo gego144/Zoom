c3b3d5a94131a27861c72425469e3e6b6fbc1cf2
gego144 <56304474+gego144@users.noreply.github.com>
2019-10-27 23:49:04 -0300

    Create trackmover.cs

---
+++ scripts.cs/trackmover.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trackmove : MonoBehaviour
+{
+    public float speed;
+    Vector2 offset;
+
+   
+    void Update()
+    {
+        offset = new Vector2(0, Time.time * speed);
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+
+    }
+}
