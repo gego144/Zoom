3cb6aa7f3f64305e6e7998cc52148177e741eb42
gego144 <56304474+gego144@users.noreply.github.com>
2019-10-27 23:47:19 -0300

    Create carcontroller.cs

---
+++ scripts.cs/carcontroller.cs
@@ -0,0 +1,37 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carcontroller : MonoBehaviour
+{
+    public float carspeed;
+    public float maxpos = 2.2f;
+    
+    
+    Vector3 position;
+
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        position.x += Input.GetAxis("Horizontal") * carspeed * Time.deltaTime;
+
+        position.x = Mathf.Clamp(position.x, -2.2f, 2.2f);
+
+        transform.position = position;
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
