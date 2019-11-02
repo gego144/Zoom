198cb76caa9f9b9ee7318035db1a621a9e56d83c
gego144 <56304474+gego144@users.noreply.github.com>
2019-11-02 00:34:11 -0300

    Update uiManager.cs

--- scripts.cs/uiManager.cs
+++ scripts.cs/uiManager.cs
@@ -1,19 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class uiManager : MonoBehaviour
 {
 
+    public Text scoreText;
+    bool gameOver;
+    int score;
+
+
+    void Start()
+    {
+        gameOver = false;
+        score = 0;
+        InvokeRepeating("scoreUpdate", 1.0f, 1.0f);
+    }
+
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    void scoreUpdate()
+    {
+        if (gameOver == false)
+        {
+            score += 1;
+        }
+        
+    }
+
+
+    public void gameOverOn()
+    {
+        gameOver = true;
+    }
+
 
     public void Play()
     {
-        Application.LoadLevel("Level1");
+        SceneManager.LoadScene(1);
     }
     
-    
-    
-    
+
     public void Pause()
     {
         if(Time.timeScale == 1)
@@ -26,5 +58,16 @@ public class uiManager : MonoBehaviour
         }
     
     }
+
+    public void Menu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
 
 }
